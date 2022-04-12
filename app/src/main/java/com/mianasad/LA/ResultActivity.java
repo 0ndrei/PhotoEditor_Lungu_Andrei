37f7e221a4e5254098a3cad99ec89f2a7be6a9a3
0ndrei <lungu.andrei.00@mail.ru>
2022-04-12 19:20:40 +0300

    Add bt back to home.

--- app/src/main/java/com/mianasad/LA/ResultActivity.java
+++ app/src/main/java/com/mianasad/LA/ResultActivity.java
@@ -2,7 +2,10 @@ package com.mianasad.LA;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.mianasad.LA.databinding.ActivityResultBinding;
 
@@ -10,6 +13,9 @@ import com.mianasad.LA.databinding.ActivityResultBinding;
 public class ResultActivity extends AppCompatActivity {
 
     ActivityResultBinding binding;
+    int IMAGE_REQUEST_CODE = 45;
+    int CAMERA_REQUEST_CODE = 14;
+    int RESULT_CODE = 200;
 
 
     @Override
@@ -22,5 +28,17 @@ public class ResultActivity extends AppCompatActivity {
 
         binding.image.setImageURI(getIntent().getData());
 
+        ImageView imageView = (ImageView) findViewById(R.id.homeBtn);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                homePage();
+            }
+
+            private void homePage() {
+                Intent intent = new Intent(ResultActivity.this, MainActivity.class);
+                startActivity(intent);
+          }
+        });
     }
 }
